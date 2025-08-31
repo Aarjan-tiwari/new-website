83e111d3a2571f8541cd3f2d6c54f22e7133a385
Aarjan <150925578+Aarjan-tiwari@users.noreply.github.com>
2025-09-01 02:08:14 +1000

    Create favicon.js

---
+++ app/favicon.js
@@ -0,0 +1,6 @@
+// This is a placeholder for the favicon configuration
+// The actual favicon will be generated during build time or manually added to the public folder
+// For now, we'll use a simple configuration that works with a circular favicon
+
+// This file is intentionally left empty as the favicon is configured in layout.tsx
+// and the actual favicon.ico file should be placed in the public folder
