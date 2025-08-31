ceeb38be6ef40a99b3fe885f025af6ea93c45d0a
Aarjan <150925578+Aarjan-tiwari@users.noreply.github.com>
2025-09-01 00:14:06 +1000

    Update basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/new-website',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,6 +11,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Optional: Add assetPrefix if you're using a custom domain
+  // assetPrefix: '/',
 }
 
 export default nextConfig
